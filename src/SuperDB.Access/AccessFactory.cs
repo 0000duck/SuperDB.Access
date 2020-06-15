125ed4ea1849d4dd2203479a635494ac642aafa4
wangzhen <zzuwangzhen@outlook.com>
2020-06-15 16:48:12 +0800

    1.0.0

---
+++ src/SuperDB.Access/AccessFactory.cs
@@ -0,0 +1,33 @@
+﻿using SuperDB.Config;
+
+using System.Data;
+using System.Data.Odbc;
+
+namespace SuperDB.Access
+{
+    public class AccessFactory : DBFactory
+    {
+        private IDbConnection _Connection;
+
+        public static AccessFactory Create()
+        {
+            return new AccessFactory();
+        }
+
+        public override IDbConnection Connection
+        {
+            get
+            {
+                if (_Connection == default)
+                {
+                    _Connection = new OdbcConnection(DBConfig.ConnectionString);
+                }
+                if (_Connection.State != ConnectionState.Open)
+                {
+                    _Connection.Open();
+                }
+                return _Connection;
+            }
+        }
+    }
+}
